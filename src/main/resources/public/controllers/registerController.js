af31155ab6f0d8d07a96bc6f583a4da68e056224
unknown <mollison@PC20141.catalystsolves.com>
2015-12-04 15:57:52 -0800

    adding more to view controllers to use http calls

--- src/main/resources/public/controllers/registerController.js
+++ src/main/resources/public/controllers/registerController.js
@@ -1,5 +1,39 @@
-angular.module('myApp').controller('registerCtrl',['$scope', 'registerFactory',
-   function($scope, registerFactory) {
+angular.module('myApp').controller('registerCtrl', ['$scope', 'usersFactory', 'registerFactory',
+  function($scope, usersFactory){
+
+	$scope.username;
 	
-    }
-]);
+	$scope.password;
+	
+	$scope.confirmPassword;
+	
+	$scope.user = {};
+	$scope.user.password = $scope.password;
+	$scope.user.username = $scope.username;
+
+	$scope.getUsers = function(){
+	
+		usersFactory.getUsers().then(
+			function(results){
+				$scope.results = results;
+			},
+			function(error){
+				console.log(error);
+			}
+		);
+	};
+	$scope.addUser = function($scope.user){
+		
+		usersFactory.addUsers().then(
+			function(results){
+				$scope.results = results;
+			},
+			function(error){
+				console.log(error);
+			}
+		);
+	};
+	
+
+
+}]);
--- src/main/resources/public/factories/usersFactory.js
+++ src/main/resources/public/factories/usersFactory.js
@@ -4,24 +4,13 @@ angular.module('myApp').factory('usersFactory', ['$http', function($http){
 	
 		getUsers: function(){
 		
-			return $http.get("localhost:8080/users/get").then(
-				function(response){
-				
-					return response;
-					
-				},
-				function(error){
-				
-					return error;
-					
-				}
-			);
+			return $http.get("localhost:8080/users/get");
 		
 		},
 	
 		addUser: function(searchQuery){
 		
-			return $http.post("localhost:8080/users/get").then();
+			return $http.post("localhost:8080/users/get");
 		
 		}
 	
