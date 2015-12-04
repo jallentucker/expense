593380b111087584e533cbb6a913ff012704aea7
Cody Miller <cmiller@catalystitservices.com>
2015-12-04 13:15:24 -0800

    User entity created, working on the Date part of it

---
+++ src/main/java/com/catalyst/tla_expense/entities/User.java
@@ -0,0 +1,67 @@
+package com.catalyst.tla_expense.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="users", schema="public")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	@Column(name="user_password")
+	private String userPassword;
+	
+	
+	@NotNull
+	@Column(name="user_email", unique = true)
+	private String userEmail;
+	
+	@Column(name="create_date", columnDefinition="TIME")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createDate;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+}
