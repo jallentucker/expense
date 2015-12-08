ac2ce11453e9ff7a7304dd7d1d8e64504738aa5d
Joshua Tucker <jtucker@catalystitservices.com>
2015-12-08 14:13:56 -0800

    changed user<attribute> references to user.user<attribute>

--- src/main/resources/public/controllers/registerController.js
+++ src/main/resources/public/controllers/registerController.js
@@ -22,8 +22,6 @@ angular.module('myApp').controller('registerCtrl', ['$scope', 'usersFactory', 'v
 		);
 	};
 	$scope.addUser = function(user){
-		console.log('user: ');
-		console.log(user);
 		if($scope.validateUser(user)) {
 			usersFactory.addUser().then(
 				function(results){
@@ -45,27 +43,18 @@ angular.module('myApp').controller('registerCtrl', ['$scope', 'usersFactory', 'v
 	$scope.validateEmail = function(email, persist) {
 		if (!validationFactory.validateField(emailRegex, persist, email)) {
 			$scope.emailIsInvalid = true;
-			console.log('emailIsInvalid');
 		} else {
 			$scope.emailIsInvalid = false;
 		}
-		console.log(validationFactory.validateField(emailRegex, true, 'h i@gmail.com'));
 	};
 	$scope.validatePassword = function(password, persist) {
-		console.log('password:' + password);
-		console.log('persist:' + persist);
 		if (!validationFactory.validateField(passwordRegex, persist, password)) {
 			$scope.passwordIsInvalid = true;
-			console.log('passwordIsInvalid');
 		} else {
 			$scope.passwordIsInvalid = false;
 		}
-		console.log(validationFactory.validateField(passwordRegex, true, 'Q!1q qqq'));
 	};
 	$scope.validateUser = function(user) {
-		// $scope.passwordsDiffer = !$scope.confirmPassword(user.userPassword, user.confirmUserPassword);
-		// $scope.emailIsInvalid = !$scope.validateEmail(user.userEmail);
-		// $scope.passwordIsInvalid = !$scope.validatePassword(user.userPassword);
 		$scope.confirmPassword(user.userPassword, user.confirmUserPassword);
 		$scope.validateEmail(user.userEmail, true);
 		$scope.validatePassword(user.userPassword, true);
