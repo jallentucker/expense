cf47700b44b16fc71be7f3a0b107da089d263223
Joshua Tucker <jtucker@catalystitservices.com>
2015-12-08 10:43:08 -0800

    email validation now checks for a period

--- src/main/resources/public/values/regexValues.js
+++ src/main/resources/public/values/regexValues.js
@@ -1,2 +1,2 @@
-angular.module('myApp').value('emailRegex', '^.{1,}@.{1,}\..{1,}$');
+angular.module('myApp').value('emailRegex', '^.{1,}@.{1,}[.]{1}.{1,}$');
 angular.module('myApp').value('passwordRegex', '^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$');
