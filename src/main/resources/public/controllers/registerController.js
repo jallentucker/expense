f2b647645e9b2fe3c1518d6932069b1821ada20b
Joshua Tucker <jtucker@catalystitservices.com>
2015-12-08 13:55:07 -0800

    debugging

--- src/main/resources/public/controllers/registerController.js
+++ src/main/resources/public/controllers/registerController.js
@@ -6,9 +6,9 @@ angular.module('myApp').controller('registerCtrl', ['$scope', 'usersFactory', 'v
 	$scope.password;
 
 	$scope.user = {};
-	$scope.user.userPassword = $scope.userPassword;
-	$scope.user.userEmail = $scope.userEmail;
-	$scope.user.confirmUserPassword = $scope.confirmUserPassword;
+	$scope.user.userPassword;
+	$scope.user.userEmail;
+	$scope.user.confirmUserPassword;
 
 	$scope.getUsers = function(){
 	
@@ -22,6 +22,8 @@ angular.module('myApp').controller('registerCtrl', ['$scope', 'usersFactory', 'v
 		);
 	};
 	$scope.addUser = function(user){
+		console.log('user: ');
+		console.log(user);
 		if($scope.validateUser(user)) {
 			usersFactory.addUser().then(
 				function(results){
@@ -40,24 +42,33 @@ angular.module('myApp').controller('registerCtrl', ['$scope', 'usersFactory', 'v
 			$scope.passwordsDiffer = false;
 		}
 	};
-	$scope.validateEmail = function(email) {
-		if (!validationFactory.validateField(emailRegex, false, email)) {
+	$scope.validateEmail = function(email, persist) {
+		if (!validationFactory.validateField(emailRegex, persist, email)) {
 			$scope.emailIsInvalid = true;
+			console.log('emailIsInvalid');
 		} else {
 			$scope.emailIsInvalid = false;
 		}
+		console.log(validationFactory.validateField(emailRegex, true, 'h i@gmail.com'));
 	};
-	$scope.validatePassword = function(password) {
-		if (!validationFactory.validateField(passwordRegex, false, password)) {
+	$scope.validatePassword = function(password, persist) {
+		console.log('password:' + password);
+		console.log('persist:' + persist);
+		if (!validationFactory.validateField(passwordRegex, persist, password)) {
 			$scope.passwordIsInvalid = true;
+			console.log('passwordIsInvalid');
 		} else {
 			$scope.passwordIsInvalid = false;
 		}
+		console.log(validationFactory.validateField(passwordRegex, true, 'Q!1q qqq'));
 	};
 	$scope.validateUser = function(user) {
-		$scope.passwordsDiffer = !$scope.confirmPassword(user.userPassword, user.confirmUserPassword);
-		$scope.emailIsInvalid = !$scope.validateEmail(user.userEmail);
-		$scope.passwordIsInvalid = !$scope.validatePassword(user.userPassword);
+		// $scope.passwordsDiffer = !$scope.confirmPassword(user.userPassword, user.confirmUserPassword);
+		// $scope.emailIsInvalid = !$scope.validateEmail(user.userEmail);
+		// $scope.passwordIsInvalid = !$scope.validatePassword(user.userPassword);
+		$scope.confirmPassword(user.userPassword, user.confirmUserPassword);
+		$scope.validateEmail(user.userEmail, true);
+		$scope.validatePassword(user.userPassword, true);
 		if ($scope.passwordsDiffer || $scope.emailIsInvalid || $scope.passwordIsInvalid) {
 			return false;
 		} else {
