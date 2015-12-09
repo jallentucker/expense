9004cf0b57606606645cd8c8c05741b5e45bb810
unknown <mollison@PC20141.catalystsolves.com>
2015-12-09 09:13:16 -0800

    finished login selenium test

--- src/main/java/com/catalyst/tla_expense/SeleniumFramework/TestPageObject.java
+++ src/main/java/com/catalyst/tla_expense/SeleniumFramework/TestPageObject.java
@@ -8,13 +8,11 @@ import org.junit.Before;
 public abstract class TestPageObject {
     protected WebDriver driver;
     SeleniumSettings seleniumSettings;
-
     @Before
     public void beforeAllSeleniumTests(){
         seleniumSettings = new SeleniumSettings();
         driver = seleniumSettings.getDriver();
     }
-
     @After
     public void afterAllSeleniumTests(){
         driver.quit();
