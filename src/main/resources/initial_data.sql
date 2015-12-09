d4169f0393419f9df4abae6427ebe7b92e5d21ae
Alysha <arecore@catalystdevworks.com>
2015-12-09 13:35:07 -0800

    fixed minor looks

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -6,3 +6,6 @@ INSERT INTO status(status_type) VALUES ('Submitted');
 INSERT INTO status(status_type) VALUES ('Rejected');
 INSERT INTO status(status_type) VALUES ('Saved');
 INSERT INTO status(status_type) VALUES ('Under Review');
+
+INSERT INTO project(project_name, approver_id) VALUES ('pie maker', 1);
+INSERT INTO project(project_name, approver_id) VALUES ('beef baster', 2);
