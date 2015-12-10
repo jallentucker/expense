cd0f013ae7c1052af3f5d2ba2338bfd99b5c3e76
unknown <mollison@PC20141.catalystsolves.com>
2015-12-10 10:35:02 -0800

    added selenium register test

--- src/test/java/com/catalyst/tla_expense/PageObjectFramework/Pages/LoginPageTest.java
+++ src/test/java/com/catalyst/tla_expense/PageObjectFramework/Pages/LoginPageTest.java
@@ -17,35 +17,24 @@ public class LoginPageTest extends TestPageObject{
 
 @Test
 public void checkThatItGoesToTheRightPage(){
-
     LoginPage login = new LoginPage(driver);
-
     String actualURL = login.getUrl();
-
     assertEquals("http://localhost:8080/login", actualURL);
 }
 
 @Test
 public void checkThatThePageGetsTheTitle(){
-
     LoginPage login = new LoginPage(driver);
-
     String actualtitle = login.getTitle();
-
     assertEquals("Expenses Application", actualtitle);
 }
 
 @Test
 public void checkThatValidUserNameAndPasswordNavigatesToHomePage(){
-
     LoginPage login = new LoginPage(driver);
-
     login.sendKeys(By.id("username"), "dummy");
     login.sendKeys(By.id("password"), "password1");
-    
-
     login.click(By.id("loginSubmit"));
-
     String actualURL = login.getUrl();
     assertEquals("http://localhost:8080/#/home", actualURL);
 }
