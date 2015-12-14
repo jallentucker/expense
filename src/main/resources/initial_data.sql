42784a979592a74cdeb5e46fa3f85d79d0e6cb77
Jacob Miller <jmiller@catalystitservices.com>
2015-12-14 10:24:01 -0800

    Created a consistent user for selenium testing.

--- src/main/resources/initial_data.sql
+++ src/main/resources/initial_data.sql
@@ -1,5 +1,4 @@
-INSERT INTO expense_user(user_email, user_password) VALUES ('dummy', 'password1')
-INSERT INTO expense_user(user_email, user_password) VALUES ('dummy2', 'password2')
+INSERT INTO expense_user(user_email, user_password) VALUES ('dummy@gmail.com', '$2a$10$F.tNwMhgMazE4V49BJRN/eY86pqfIvRSabhnQ0IJePqd/sPV7a98a');
 
 INSERT INTO status(status_type) VALUES ('Approved');
 INSERT INTO status(status_type) VALUES ('Submitted');
