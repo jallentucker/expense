0f4ac59bddb400b7fe139640d23e5bdf90013c90
Cody Miller <cmiller@catalystitservices.com>
2015-12-16 11:38:17 -0800

    fixed selenium waits

--- src/main/java/com/catalyst/tla_expense/SeleniumFramework/Pages/HomePage.java
+++ src/main/java/com/catalyst/tla_expense/SeleniumFramework/Pages/HomePage.java
@@ -1,7 +1,6 @@
 package com.catalyst.tla_expense.SeleniumFramework.Pages;
 
 import com.catalyst.tla_expense.SeleniumFramework.PageObject;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends PageObject {
