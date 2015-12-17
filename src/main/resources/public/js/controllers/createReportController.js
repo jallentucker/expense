19adac7bc3185aab84b172b8a3c6d217908d29f8
Alysha <arecore@catalystdevworks.com>
2015-12-17 11:48:51 -0800

    fixed saved button to show when required items are filled out

--- src/main/resources/public/js/controllers/createReportController.js
+++ src/main/resources/public/js/controllers/createReportController.js
@@ -10,25 +10,34 @@ angular.module('myApp').controller('createReportController',['$scope', '$http', 
     		function(error){
     			$scope.currentUser = error;
     	});
-
-    $scope.submitReport = function(report, selectedProject){
-    		report.user = $scope.currentUser;
-            report.project = selectedProject;
-            report.status = {statusName: "submitted"};
-            if(report.reportName.length > 2) {
-            	createReportFactory.createReport(report).then(
-                    function(success){
-                        $scope.createReportResult = success;
-                        window.location = "/#/home";
-                    },
-                    function(error){
-                        $scope.createReportResult = error;
-                    });
-            }
-            else {
-            	console.log("Name didn't meet requirements");
-            }
+    
+    $scope.reqDropdown = function() {
+    	if($scope.selectedProject && $scope.selectedProject.projectName) return false;
+    	return true;
+    }
+    
+    $scope.reqLineItem = function() {
+    	
+    }
+    
+    $scope.createReport = function(report, selectedProject, status){
+		report.user = $scope.currentUser;
+        report.project = selectedProject;
+        report.status = {statusId: status};
+        if(report.reportName.length > 2) {
+        	createReportFactory.createReport(report).then(
+                function(success){
+                    $scope.createReportResult = success;
+                    window.location = "/#/home";
+                },
+                function(error){
+                    $scope.createReportResult = error;
+                });
         }
+        else {
+        	console.log("Name didn't meet requirements");
+        }
+    }
 
 	$scope.ProjectList = null;
 	$scope.fillProjectList = function() {
